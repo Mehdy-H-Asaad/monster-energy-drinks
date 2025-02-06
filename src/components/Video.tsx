c74d5b89a33e70ddd0423c06a05cec8e8860e56b
Mehdy-H-Asaad <mehdyasaad.sy.2003@gmail.com>
2025-02-06 04:21:59 +0400

    Video Poster

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,9 @@
 import monsterVideo from "../assets/imgs/Monster Energy Drink Ad.mp4";
+import Poster from "../assets/imgs/poster.png";
 export const Video = () => {
 	return (
 		<div className="video">
-			<video src={monsterVideo} loop muted autoPlay></video>
+			<video src={monsterVideo} poster={Poster} loop muted autoPlay></video>
 		</div>
 	);
 };
